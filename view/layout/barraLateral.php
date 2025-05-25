e9e559a679af562a1fa08a4e72e98d2b4e46023a
Alessandro018 <alessandrosilva325@gmail.com>
2025-05-24 22:17:42 -0300

    Layout base

---
+++ view/layout/barraLateral.php
@@ -0,0 +1,9 @@
+<nav class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active rounded-2" href="#">Início</a>
+            </li>
+        </ul>
+    </div>
+</nav>
